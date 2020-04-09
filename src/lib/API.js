1e33a324d2b116d1fa5064424e146a6d0fbf1a00
Lilian Bemelmans <56588573+LilianBemelmans@users.noreply.github.com>
2020-04-09 10:07:00 +0200

    Update API.js

--- src/lib/API.js
+++ src/lib/API.js
@@ -6,9 +6,18 @@ export default class API{
         .then(response => response.json())
         .then(data => {
             resolve(data);
-            })
+        })
         .catch(error => {
             reject(error)
-            });
         });
-    }
+    });
+
+    static saveData = () => new Promise ( (resolve, reject) => {
+        
+        fetch('https://api.dev-master.ninja/reactjs/smoelenboek/post/')
+        .then(response => response.json())
+        .then(data => { console.log(data); })
+        .catch( error => {console.log(error);});
+        alert("Data has been uploaded")
+    });
+}
